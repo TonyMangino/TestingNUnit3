c122dbfc3daafb5adcb212bb8ae3474ac59aec45
Tony Mangino <tm13089@gmail.com>
2021-01-11 17:25:49 -0500

    Updated ComparisonTests.
    Added CompoundTests.

--- ComparisonTests.cs
+++ ComparisonTests.cs
@@ -10,14 +10,85 @@ namespace NUnit3Tests
         private bool meaningOfLife = 42 > 0;
 
         [Test]
-        [Category("Boolean test")]
-        public void BooleanTest()
+        public void GreaterThanTest()
+        {
+            //GreaterThanConstraint tests that one value is greater than another.
+
+            //Modifiers
+            //...Using(IComparer comparer)
+            //...Using<T>(IComparer<T> comparer)
+            //...Using<T>(Comparison<T> comparer)
+
+            Assert.That(result, Is.GreaterThan(2));
+            Assert.That(7, Is.GreaterThan(3));
+            //Assert.That(myOwnObject, Is.GreaterThan(theExpected).Using(myComparer));
+        }
+
+        [Test]
+        public void GreaterThanOrEqualTest()
+        {
+            //GreaterThanOrEqualConstraint tests that one value is greater than or equal to another.
+
+            //Modifiers
+            //...Using(IComparer comparer)
+            //...Using<T>(IComparer<T> comparer)
+            //...Using<T>(Comparison<T> comparer)
+
+            Assert.That(result, Is.GreaterThanOrEqualTo(42));
+            Assert.That(7, Is.GreaterThanOrEqualTo(3));
+            Assert.That(7, Is.AtLeast(3));
+            Assert.That(7, Is.GreaterThanOrEqualTo(7));
+            Assert.That(7, Is.AtLeast(7));
+            //Assert.That(myOwnObject, Is.GreaterThanOrEqualTo(theExpected).Using(myComparer));
+        }
+
+        [Test]
+        public void LessThanTest()
+        {
+            //LessThanConstraint tests that one value is less than another.
+
+            //Modifiers
+            //...Using(IComparer comparer)
+            //...Using<T>(IComparer<T> comparer)
+            //...Using<T>(Comparison<T> comparer)
+
+            Assert.That(result, Is.LessThan(49));
+            Assert.That(3, Is.LessThan(7));
+            Assert.That(-5, Is.Negative);
+            //Assert.That(myOwnObject, Is.LessThan(theExpected).Using(myComparer));
+        }
+
+        [Test]
+        public void LessThanOrEqualToTest()
+        {
+            //LessThanOrEqualConstraint tests that one value is less than or equal to another.
+
+            //Modifiers
+            //...Using(IComparer comparer)
+            //...Using<T>(IComparer<T> comparer)
+            //...Using<T>(Comparison<T> comparer)
+
+            Assert.That(result, Is.LessThanOrEqualTo(42));
+            Assert.That(3, Is.LessThanOrEqualTo(7));
+            Assert.That(3, Is.AtMost(7));
+            Assert.That(3, Is.LessThanOrEqualTo(3));
+            Assert.That(3, Is.AtMost(3));
+            //Assert.That(myOwnObject, Is.LessThanOrEqualTo(theExpected).Using(myComparer));
+        }
+
+        [Test]
+        public void IsTrueTest()
         {
             Assert.That(meaningOfLife, Is.True);
         }
 
         [Test]
-        [Category("Equality test")]
+        public void IsPositiveTest()
+        {
+            Assert.That(result, Is.Positive);
+        }
+
+        [Test]
         public void EqualityTest()
         {
             Assert.That(result, Is.EqualTo(42));
@@ -25,33 +96,27 @@ namespace NUnit3Tests
         }
 
         [Test]
-        [Category("Equality test")]
-        public void GreaterThanTest()
-        {
-            Assert.That(result, Is.GreaterThan(2));
-            Assert.That(result, Is.GreaterThanOrEqualTo(42));
-        }
-
-        [Test]
-        [Category("Equality test")]
-        public void LessThanTest()
-        {
-            Assert.That(result, Is.LessThan(49));
-            Assert.That(result, Is.LessThanOrEqualTo(42));
-        }
-
-        [Test]
-        [Category("Range test")]
         public void RangeTest()
         {
+            //RangeConstraint tests that a value is in an (inclusive) range.
+
+            //Modifiers
+            //...Using(IComparer comparer)
+            //...Using<T>(IComparer<T> comparer)
+            //...Using<T>(Comparison<T> comparer)
+
+            int[] intArray = new int[] { 1, 2, 3 };
+
             Assert.That(result, Is.InRange(40, 80));
             Assert.That(result, Is.Not.InRange(20, 40));
             Assert.That(result, Is.GreaterThan(4).And.LessThan(50));
             Assert.That(result, Is.LessThan(10).Or.GreaterThan(40));
+            Assert.That(42, Is.InRange(1, 100));
+            Assert.That(intArray, Is.All.InRange(1, 3));
+            //Assert.That(myOwnObject, Is.InRange(lowExpected, highExpected).Using(myComparer));
         }
 
         [Test]
-        [Category("Reference equality test")]
         public void ReferenceEquality()
         {
             var a = new List<string> { "a", "b" };
---
+++ CompoundTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace NUnit3Tests
+{
+    [Category("Compound tests")]
+    public class CompoundTests
+    {
+        [Test]
+        public void AndTest()
+        {
+            //AndConstraint combines two other constraints and succeeds only if they both succeed.
+
+            //Note that the constraint evaluates the sub-constraints left to right.
+            //The OrConstraint has precedence over the AndConstraint.
+
+            Assert.That(2.3, Is.GreaterThan(2.0).And.LessThan(3.0));
+        }
+
+        [Test]
+        public void NotTest()
+        {
+            //NotConstraint reverses the effect of another constraint. If the base constraint fails, 
+            //NotConstraint succeeds. If the base constraint succeeds, NotConstraint fails.
+
+            int[] array = new int[] { 1, 2, 2, 3, 4, 5 };
+
+            Assert.That(array, Is.Not.Unique);
+            Assert.That(2 + 2, Is.Not.EqualTo(5));
+        }
+
+        [Test]
+        public void OrTest()
+        {
+            //OrConstraint combines two other constraints and succeeds if either of them succeeds.
+
+            //Note that the constraint evaluates the sub-constraints left to right.
+            //The OrConstraint has precedence over the AndConstraint.
+
+            Assert.That(3, Is.LessThan(5).Or.GreaterThan(10));
+        }
+    }
+}
