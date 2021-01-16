0a0e3111339c3500fd08c20b27dcf9f5a10591a3
Tony Mangino <tm13089@gmail.com>
2021-01-16 10:38:40 -0500

    Updated Directory and File tests.
    Updated String tests.

--- DirectoryFileTests.cs
+++ DirectoryFileTests.cs
@@ -24,15 +24,17 @@ namespace NUnit3Tests
         }
 
         [Test]
-        public void FileExistsTest()
+        public void FileOrDirectoryExistsTest()
         {
+            //FileOrDirectoryExistsConstraint tests that a File or Directory exists.
+
+            Assert.That(fileReal, Does.Exist);
+            Assert.That(directoryReal, Does.Exist);
+            Assert.That(fileFake, Does.Not.Exist);
+            Assert.That(directoryFake, Does.Not.Exist);
+
             Assert.That(new FileInfo(fileReal), Does.Exist);
             Assert.That(new FileInfo(fileFake), Does.Not.Exist);
-        }
-
-        [Test]
-        public void DirectoryExistsTest()
-        {
             Assert.That(new DirectoryInfo(directoryReal), Does.Exist);
             Assert.That(new DirectoryInfo(directoryFake), Does.Not.Exist);
         }
@@ -40,7 +42,73 @@ namespace NUnit3Tests
         [Test]
         public void SamePathTest()
         {
-            Assert.That(directoryEmpty, Is.SamePath(@"d:\junk\foo").IgnoreCase);
+            //SamePathConstraint tests that two paths are equivalent.
+
+            //Path constraints perform tests on paths, without reference to any actual files 
+            //or directories. This allows testing paths that are created by an application for 
+            //reference or later use, without any effect on the environment.
+
+            //Path constraints are intended to work across multiple file systems, 
+            //and convert paths to a canonical form before comparing them.
+
+            //It is usually not necessary to know the file system of the paths in order to 
+            //compare them. Where necessary, the programmer may use the IgnoreCase and RespectCase 
+            //modifiers to provide behavior other than the system default.
+
+            Assert.That("/folder1/./junk/../folder2", Is.SamePath("/folder1/folder2"));
+            Assert.That("/folder1/./junk/../folder2/x", Is.Not.SamePath("/folder1/folder2"));
+
+            Assert.That(@"C:\folder1\folder2", Is.SamePath(@"C:\Folder1\Folder2").IgnoreCase);
+            Assert.That("/folder1/folder2", Is.Not.SamePath("/Folder1/Folder2").RespectCase);
+        }
+
+        [Test]
+        public void SamePathOrUnderTest()
+        {
+            //SamePathOrUnderConstraint tests that one path is equivalent to another path or that it is under it.
+
+            //Path constraints perform tests on paths, without reference to any actual files 
+            //or directories. This allows testing paths that are created by an application 
+            //for reference or later use, without any effect on the environment.
+
+            //Path constraints are intended to work across multiple file systems, 
+            //and convert paths to a canonical form before comparing them.
+
+            //It is usually not necessary to know the file system of the paths in order to 
+            //compare them. Where necessary, the programmer may use the IgnoreCase and RespectCase 
+            //modifiers to provide behavior other than the system default.
+
+            Assert.That("/folder1/./junk/../folder2", Is.SamePathOrUnder("/folder1/folder2"));
+            Assert.That("/folder1/junk/../folder2/./folder3", Is.SamePathOrUnder("/folder1/folder2"));
+            Assert.That("/folder1/junk/folder2/folder3", Is.Not.SamePathOrUnder("/folder1/folder2"));
+
+            Assert.That(@"C:\folder1\folder2\folder3", Is.SamePathOrUnder(@"C:\Folder1\Folder2").IgnoreCase);
+            Assert.That("/folder1/folder2/folder3", Is.Not.SamePathOrUnder("/Folder1/Folder2").RespectCase);
+        }
+
+        [Test]
+        public void SubPathTest()
+        {
+            //SubPathConstraint tests that one path is under another path.
+
+            //Path constraints perform tests on paths, without reference to any actual files 
+            //or directories. This allows testing paths that are created by an application 
+            //for reference or later use, without any effect on the environment.
+
+            //Path constraints are intended to work across multiple file systems, 
+            //and convert paths to a canonical form before comparing them.
+
+            //It is usually not necessary to know the file system of the paths in order to 
+            //compare them. Where necessary, the programmer may use the IgnoreCase and RespectCase 
+            //modifiers to provide behavior other than the system default.
+
+            //NOTE: The examples from the NUnit website used SubPath(), which produced errors.
+
+            Assert.That("/folder1/folder2/foo", Is.SubPathOf("/folder1/folder2"));
+            Assert.That("/folder1/junk/folder2", Is.Not.SubPathOf("/folder1/folder2"));
+
+            Assert.That(@"C:\folder1\folder2\folder3", Is.SubPathOf(@"C:\Folder1\Folder2").IgnoreCase);
+            Assert.That("/folder1/folder2/folder3", Is.Not.SubPathOf("/Folder1/Folder2/Folder3").RespectCase);
         }
     }
 }
--- StringTests.cs
+++ StringTests.cs
@@ -9,7 +9,6 @@ namespace NUnit3Tests
         private readonly string nuttin = string.Empty;
 
         [Test]
-        [Category("Equality test")]
         public void EqualityTest()
         {
             Assert.That(result, Is.EqualTo("foobar"));
@@ -18,43 +17,74 @@ namespace NUnit3Tests
         }
 
         [Test]
-        [Category("Substring test")]
         public void SubstringTest()
         {
+            //SubstringConstraint tests for a substring.
+
+            string phrase = "Make your tests fail before passing!";
+
+            Assert.That(phrase, Does.Contain("tests fail"));
+            Assert.That(phrase, Does.Not.Contain("tests pass"));
+            Assert.That(phrase, Does.Contain("make").IgnoreCase);
+
             Assert.That(result, Does.Contain("oba").IgnoreCase);
             Assert.That(result, Does.Not.Contain("lmn").IgnoreCase);
         }
 
         [Test]
-        [Category("Empty test")]
         public void EmptyTest()
         {
+            //The EmptyStringConstraint tests if a string is empty.
+
+            //Is.Empty actually creates an EmptyConstraint. Subsequently applying
+            //it to a string causes an EmptyStringConstraint to be created.
+
             Assert.That(nuttin, Is.Empty);
             Assert.That(result, Is.Not.Empty);
         }
 
         [Test]
-        [Category("Starts with test")]
         public void StartsWithTest()
         {
+            //StartsWithConstraint tests for an initial string.
+
+            //StartsWith may appear only in the body of a constraint 
+            //expression or when the inherited syntax is used.
+
             Assert.That(result, Does.StartWith("foo"));
+            Assert.That(result, Does.StartWith("FOO").IgnoreCase);
             Assert.That(result, Does.Not.StartWith("moo"));
+            Assert.That(result, Does.Not.StartWith("MOO").IgnoreCase);
         }
 
         [Test]
-        [Category("Ends with test")]
         public void EndsWithTest()
         {
+            //EndsWithConstraint tests for an ending string.
+
+            //EndsWith may appear only in the body of a constraint 
+            //expression or when the inherited syntax is used.
+
             Assert.That(result, Does.EndWith("bar"));
+            Assert.That(result, Does.EndWith("BAR").IgnoreCase);
             Assert.That(result, Does.Not.EndWith("baz"));
         }
 
         [Test]
-        [Category("Regex test")]
         public void RegexTest()
         {
+            //RegexConstraint tests that a pattern is matched.
+
+            //Matches may appear only in the body of a constraint 
+            //expression or when the inherited syntax is used.
+
+            string phrase = "Make your tests fail before passing!";
+
             Assert.That(result, Does.Match("f*r"));
             Assert.That(result, Does.Not.Match("m*n"));
+            Assert.That(phrase, Does.Match("Make.*tests.*pass"));
+            Assert.That(phrase, Does.Match("make.*tests.*PASS").IgnoreCase);
+            Assert.That(phrase, Does.Not.Match("your.*passing.*tests"));
         }
     }
 }
