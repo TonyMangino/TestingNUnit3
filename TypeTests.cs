54c8f3478efd88b54bd14755d603683931b6abee
Tony Mangino <tm13089@gmail.com>
2021-01-16 22:43:33 -0500

    Updated Type tests to include examples from NUnit site.

--- TypeTests.cs
+++ TypeTests.cs
@@ -18,31 +18,53 @@ namespace NUnit3Tests
         };
 
         [Test]
-        [Category("Instane of tests")]
         public void InstanceOfTest()
         {
+            //InstanceOfTypeConstraint tests that an object is of the type supplied or a derived type.
+
             IEmployee emp = new Employee();
 
             Assert.That(emp, Is.InstanceOf<IEmployee>());
             Assert.That(emp, Is.Not.InstanceOf<string>());
+
+            Assert.That("Hello", Is.InstanceOf(typeof(string)));
+            Assert.That(5, Is.Not.InstanceOf(typeof(string)));
+            Assert.That(5, Is.Not.InstanceOf<string>());
         }
 
         [Test]
-        [Category("Type of tests")]
-        public void TypeOfTest()
+        public void ExactTypeTest()
         {
             IEmployee emp = new Employee();
 
             Assert.That(emp, Is.TypeOf<Employee>());
+
+            Assert.That("Hello", Is.TypeOf(typeof(string)));
+            Assert.That("Hello", Is.Not.TypeOf(typeof(int)));
+
+            Assert.That("World", Is.TypeOf<string>());
         }
 
         [Test]
-        [Category("Assignable to tests")]
         public void AssignabletoTypeTest()
         {
+            //AssignableToConstraint tests that one type is assignable to another
+
             IEmployee emp = new Employee();
 
             Assert.That(emp, Is.AssignableTo<Employee>());
+
+            Assert.That("Hello", Is.AssignableTo(typeof(object)));
+            Assert.That(5, Is.Not.AssignableTo(typeof(string)));
+        }
+
+        [Test]
+        public void AssignableFromTest()
+        {
+            //AssignableFromConstraint tests that one type is assignable from another
+
+            Assert.That("Hello", Is.AssignableFrom(typeof(string)));
+            Assert.That(5, Is.Not.AssignableFrom(typeof(string)));
         }
     }
 }
